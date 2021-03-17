c44707b89c45c050eb20acce7e182472f1c39c75
Stefan Feier <stefan.feier@w7id.com>
2021-03-17 19:27:01 +0100

    Course Lesson 137

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,19 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Stefan Feier on 17/03/2021
+ */
+@Entity
+public class UnitOfMeasure {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String description;
+}
