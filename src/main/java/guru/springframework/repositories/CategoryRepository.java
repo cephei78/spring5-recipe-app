3dca5f02e59e318f88d9541b06124407a45b3e7e
Stefan Feier <stefan.feier@w7id.com>
2021-03-22 21:15:29 +0100

    Course Lesson 141

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,11 +1,13 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Stefan Feier on 22/03/2021
  */
-public interface CategoryInterface extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+  Optional<Category> findByDescription(String description);
 }
